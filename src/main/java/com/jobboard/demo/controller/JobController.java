258f2fe7902e800e26e79e6ccf5806ca11dbf5a4
Varsha-JK <vaka1685@colorado.edu>
2024-10-15 00:27:59 -0400

    Added get job by id

--- src/main/java/com/jobboard/demo/controller/JobController.java
+++ src/main/java/com/jobboard/demo/controller/JobController.java
@@ -1,9 +1,13 @@
 package com.jobboard.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +20,23 @@ public class JobController {
 
     @Autowired
     JobService jobService;
-    
+
     @GetMapping("")
     public List<Job> getAllJobs() {
         return jobService.getAllJobs();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Job>> getJobById(@PathVariable Long id) {
+        System.out.println("Fetching job with ID: " + id);
+        Optional<Job> job = jobService.getJobyById(id);
+
+        // return ResponseEntity
+        // .status(HttpStatus.OK)
+        // .body(job);
+        if (!job.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(job);
     }
 }
